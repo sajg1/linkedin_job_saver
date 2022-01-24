edadb8b69feaee980987180c6d65c4a18791764a
Steve Gossip <stephengossip@gmail.com>
2022-01-24 18:06:43 +0000

    able to access the desired linkedin search results

---
+++ main.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+chrome_driver_path = '/Users/admin/Development/chromedriver'
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+driver.get('https://www.linkedin.com/jobs/search/?f_WT=2%2C1%2C3&geoId=100752109&keywords=junior%20developer&location='
+           'Scotland%2C%20United%20Kingdom')
+
