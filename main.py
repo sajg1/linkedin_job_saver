aed45c41c94885fe2108ff8e9ba197fda34a043f
Steve Gossip <stephengossip@gmail.com>
2022-01-24 18:26:21 +0000

    able to click the signin button once page loads,redirecting to signin page.

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ driver = webdriver.Chrome(executable_path=chrome_driver_path)
 driver.get('https://www.linkedin.com/jobs/search/?f_WT=2%2C1%2C3&geoId=100752109&keywords=junior%20developer&location='
            'Scotland%2C%20United%20Kingdom')
 
+signin_button = driver.find_element(By.XPATH, '/html/body/div[3]/header/nav/div/a[2]')
+signin_button.click()
+
