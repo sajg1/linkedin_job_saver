f46d7b0271e03abf9b4a5801dcdb276716338aff
Steve Gossip <stephengossip@gmail.com>
2022-01-28 18:16:22 +0000

    ran into a few issues accessing the follow button further down the page. Firstly, I had to close the messaging popup box. Then scroll to the bottom of the right pane which then reveals the follow button in the html and makes it clickable.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 import time
 import os
 
@@ -14,7 +16,7 @@ driver.get('https://www.linkedin.com/jobs/search/?f_WT=2%2C1%2C3&geoId=100752109
 
 signin_button_one = driver.find_element(By.XPATH, '/html/body/div[3]/header/nav/div/a[2]')
 signin_button_one.click()
-
+driver.maximize_window()
 time.sleep(5)
 
 email = driver.find_element(By.NAME, 'session_key')
@@ -24,11 +26,23 @@ email.send_keys(EMAIL)
 password.send_keys(PASSWORD)
 signin_button_page_two.click()
 
-first_job_save_button = driver.find_element(By.XPATH, '/html/body/div[7]/div[3]/div[3]/div[2]/div/section[2]/div/div/'
-                                                      'div[1]/div/div[1]/div/div[2]/div[3]/div/button')
-time.sleep(5)
+# CLOSE MESSAGING POP UP
+message_pop_up = driver.find_element(By.CLASS_NAME, 'msg-overlay-bubble-header__details')
+message_pop_up.click()
+time.sleep(2)
+
+# SAVE JOB POST
+first_job_save_button = driver.find_element(By.CLASS_NAME, 'jobs-save-button')
 first_job_save_button.click()
 
-
+# SCROLL TO COMPANY SECTION OF JOB DESCRIPTION AND FOLLOW COMPANY
+time.sleep(3)
+job_description = driver.find_element(By.CLASS_NAME, 'jobs-search__right-rail')
+job_description.click()
+html = driver.find_element(By.TAG_NAME, 'html')
+html.send_keys(Keys.END)
+time.sleep(2)
+company_follow = driver.find_element(By.CLASS_NAME, 'follow')
+company_follow.click()
 
 
